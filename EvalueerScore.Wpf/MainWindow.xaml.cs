de39d0ef50aacf71c308ddb46b7f8975f00c7747
Stef Vercaemer <stefaan.vercaemer@howest.be>
2019-10-06 12:29:54 +0200

    Add files via upload

---
+++ EvalueerScore.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EvalueerScore.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            VulScores();
+        }
+
+        void VulScores()
+        {
+
+
+            cmbScore.Items.Add(1);
+            cmbScore.Items.Add(2);
+            cmbScore.Items.Add(3);
+            cmbScore.Items.Add(4);
+            cmbScore.Items.Add(5);
+        }
+
+        void ToonFeedBack()
+        {
+
+        }
+
+    }
+}
